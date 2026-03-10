4b0789660508d9e8d75804885929a3cf6815408e
emmmik <otusileemmanuel@gmail.com>
2026-03-10 17:42:53 +0100

    feat: Add Redis caching

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import database as database
 import models
 import schemas
 import base62
+import cache
 
 import url_repository
 import utils
@@ -44,10 +45,19 @@ def shorten_url(item: schemas.URLItemCreate, db: Session = Depends(database.get_
 
 @app.get("/{short_code}")
 def redirect_to_url(background_task: BackgroundTasks, url_id: int = Depends(get_valid_url_id), db: Session = Depends(database.get_db)):
+    cache_key = f"url:{url_id}"
+    cached_url = cache.redis_client.get(cache_key)
+    if cached_url:
+        print("Cached URL found")
+        background_task.add_task(cache.redis_client.set, cache_key, url_id, ex=3600)
+        return RedirectResponse(url=cached_url.decode())
+
     url_item = url_repository.get_url_by_url_id(url_id, db)
 
     if not url_item:
         raise HTTPException(status_code=404, detail="Error fetching URL")
+
+    cache.redis_client.set(cache_key, url_item.url, ex=3600)
 
     background_task.add_task(url_repository.increment_access_count, url_item.id, db)
     return RedirectResponse(url=url_item.url)
